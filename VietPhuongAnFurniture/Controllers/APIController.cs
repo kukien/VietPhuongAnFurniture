d3a9df4b6e394f55b53df2bed973baab158c4528
quangsmile <anhquanghoang@outlook.com>
2019-06-13 11:36:15 +0700

    Update lại

---
+++ VietPhuongAnFurniture/Controllers/APIController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VietPhuongAnFurniture.Data;
+using VietPhuongAnFurniture.Models;
+
+namespace VietPhuongAnFurniture.Controllers
+{
+    public class APIController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public APIController(ApplicationDbContext context)
+        {
+            _context = context;
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        [HttpGet]
+        [Route("[controller]/[action]")]
+        public async Task<List<Product>> LoadProductData()
+        {
+            var lstObj = new List<Product>();
+            lstObj = await _context.Products.Select(n => n).ToListAsync();
+            foreach (var item in lstObj)
+            {
+                var imageObj = _context.ProductImages.FirstOrDefault(i => i.ProductId == item.Id && i.Index == 1);
+                if (imageObj == null)
+                {
+                    imageObj = new ProductImage
+                    {
+                        // path default.
+                        Path = "img/content/product-04.jpg",
+                    };
+                }
+                item.GImage = imageObj;
+                item.SubTypeName = _context.ProductSubTypes.FirstOrDefault(n=>n.Id ==item.ProductSubTypeId)?.Name ??"";
+                item.TypeName = _context.ProductTypes.FirstOrDefault(n=>n.Id ==item.ProductTypeId)?.Name ??"";
+            }
+            return lstObj;
+        }
+    }
+}
--- VietPhuongAnFurniture/Models/Ulti/ViewModel.cs
+++ VietPhuongAnFurniture/Models/Ulti/ViewModel.cs
@@ -14,7 +14,8 @@ namespace VietPhuongAnFurniture.Models
         public List<Product> allSpecial { get; set; }
         public List<ProductType> allProductTypes { get; set; }
         public List<ProductSubType> allProductSubTypes { get; set; }
-
+        public List<string> allStuff { get; set; }
+        public List<string> allPrice { get; set; }
         public ProductImage GImage { get; set; }
 
     }
