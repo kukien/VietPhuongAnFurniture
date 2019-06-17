daecc90e62e4dfd9ae5b057f62b8460f73bfdba3
quangsmile <anhquanghoang@outlook.com>
2019-06-17 16:44:43 +0700

    Update LeftSidebar

---
+++ VietPhuongAnFurniture/Models/Banner.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VietPhuongAnFurniture.Models
+{
+    public class Banner : BaseModel
+    {
+        public string Text { get; set; }
+        public string ImageUrl { get; set; }
+
+    }
+}
